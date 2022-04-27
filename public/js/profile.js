8bb203f88c2509b352509dc6873a04ec4f4ceb6b
CORNELL\wh394 <wh394@cornell.edu>
2022-04-26 22:21:29 -0400

    Add prof pics

--- public/js/profile.js
+++ public/js/profile.js
@@ -16,6 +16,22 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function canContinue() {
+    const hasUsername = $('button.ui.button.green').length > 0;
+    const hasPicture = $('a.avatar img.green').length > 0;
+    if (hasUsername && hasPicture) {
+        $(".ui.big.labeled.icon.button").addClass("green")[0].scrollIntoView({ behavior: "smooth" });
+    } else {
+        if (hasUsername && !hasPicture) {
+            $(".ui.form .images")[0].scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+        }
+        $(".ui.big.labeled.icon.button").removeClass("green");
+    }
+}
+
 $(window).on("load", async function() {
     let timeout;
     let adjectiveArray = {};
@@ -27,6 +43,7 @@ $(window).on("load", async function() {
         nounArray = arrayToAlphabetDictionary(data);
     });
 
+    // Choose an initial
     $('.ui.dropdown').dropdown({
         onChange: function(value, text, $selectedItem) {
             // clear any usernames waiting to be loaded
@@ -38,16 +55,17 @@ $(window).on("load", async function() {
             const lastInitial = $('select[name="lastInitial"]').val();
 
             if (firstInitial !== '' && lastInitial !== '') {
-                // $(".ui.big.labeled.icon.button").addClass("green")[0];
                 $('button.ui.button').addClass("loading");
                 const randomNames = [];
-                for (var i = 0; i < 3; i++) {
+                while (randomNames.length < 3) {
                     const adjectives = adjectiveArray[firstInitial];
                     const adjective = adjectives[getRandomInt(0, adjectives.length)];
                     const nouns = nounArray[lastInitial];
                     const noun = nouns[getRandomInt(0, nouns.length)];
                     const randomName = `${adjective} ${noun}`;
-                    randomNames.push(randomName);
+                    if (!randomNames.includes(randomName)) {
+                        randomNames.push(randomName);
+                    }
                 }
                 timeout = setTimeout(function() {
                     $('button.ui.button').removeClass("loading");
@@ -56,41 +74,79 @@ $(window).on("load", async function() {
                     }
                 }, 750);
             }
+            canContinue();
         }
     });
 
-    // Click a username
+    // Choose a full username
     $('button.ui.button').on('click', function() {
         // only allow selection if there are values loaded into the buttons
         if ($(this).find("h2") && $(this).find("h2").text().trim() == '') {
-            console.log($(this).find("h2").text());
             return;
         }
+        // clear any usernames selected
         $('button.ui.button').removeClass("green");
         $('button.ui.button h2 i.check.icon.green').remove();
+
         $(this).addClass("green");
         $(this).find("h2").prepend('<i class="check icon green hidden"></i>');
         $(this).find("h2").append('<i class="check icon green"></i>');
+
+        canContinue();
+
+        if ($('.ui.warning.message.username').is(":visible")) {
+            $('.ui.warning.message.username').hide();
+        }
     })
 
     // Click a photo
     $('a.avatar').on('click', function() {
         // clear any photos selected 
-        $("a.avatar i.icon.green.check").addClass("hidden");
-        // $("a.avatar i.icon.green.check").remove();
         $('img').removeClass("green");
+        $("a.avatar i.icon.green.check").addClass("hidden");
 
         $(this).find('img').addClass("green");
         $(this).find('i.icon').removeClass("hidden");
-        // $(this).append('<i class="check icon green"></i>');
+
+        canContinue();
+
+        if ($('.ui.warning.message.photo').is(":visible")) {
+            $('.ui.warning.message.photo').hide();
+        }
     })
 
     $(".ui.big.labeled.icon.button").on('click', function() {
-        const firstName = $("span.firstName").text();
-        const lastName = $("span.lastName").text();
-        window.sessionStorage.setItem('Username', `${firstName} ${lastName}`);
+        const username = $('button.ui.button.green h2').text();
+        window.sessionStorage.setItem('Username', username);
+
+        const src = $('a.avatar img.green').attr('src');
+        window.sessionStorage.setItem('Photo', src);
         if ($(this).hasClass("green")) {
             window.location.href = '/test_misinformation';
+        } else {
+            if (username === undefined || username.trim() === '') {
+                if ($('.ui.warning.message.username').is(":hidden")) {
+                    $('.ui.warning.message.username').show();
+                    $('.ui.warning.message.username').removeClass("hidden");
+                }
+            }
+            if (src === undefined || src.trim() === '') {
+                if ($('.ui.warning.message.photo').is(":hidden")) {
+                    $('.ui.warning.message.photo').show();
+                    $('.ui.warning.message.photo').removeClass("hidden");
+                }
+            }
+            $('.ui.warning.message')[0].scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
         }
     });
+
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade');
+        });
 });
