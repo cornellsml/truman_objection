ca93ac38170dca8876bd53fc29951ed6efa54ae1
Winice Hui <wh394@cornell.edu>
2023-04-07 13:26:24 -0700

    Redirect user from profile -> test page

--- public/js/profile.js
+++ public/js/profile.js
@@ -109,7 +109,7 @@ $(window).on("load", async function() {
                 .done(function(json) {
                     const queryParams = window.location.search;
                     if (json["result"] === "success") {
-                        window.location.href = '/feed' + queryParams;
+                        window.location.href = '/test2' + queryParams;
                     } else {
                         // Should never happen, but if it does
                         const query_string = "/" + queryParams;
